711cce7badb82bbb0179fcc447b86f7e9b9caa92
Kat <21105989+ksolis3197@users.noreply.github.com>
2024-08-21 13:14:15 -0400

    Create 04_n_grams.py
    solution added

---
+++ 04_n_grams.py
@@ -0,0 +1,18 @@
+import nltk
+from nltk import word_tokenize
+from nltk.util import ngrams
+from collections import Counter
+nltk.download('punkt')
+sample_text = "I am learning NLP( Natural Lanaguage Processing)"
+tokens = word_tokenize(sample_text.lower())
+# Unigram
+unigrams = list(ngrams(tokens, 1))
+print("Unigrams:", unigrams)
+
+# Bigram
+bigrams = list(ngrams(tokens, 2))
+print("Bigrams:", bigrams)
+
+# Trigram
+trigrams = list(ngrams(tokens, 3))
+print("Trigrams:", trigrams)
