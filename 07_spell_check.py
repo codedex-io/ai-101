0f5b020cc6bde1d42f46c22f6c7ea34f532d1a97
Kat <21105989+ksolis3197@users.noreply.github.com>
2024-08-21 13:19:16 -0400

    Create 07_spell_check.py

---
+++ 07_spell_check.py
@@ -0,0 +1,6 @@
+from textblob import TextBlob
+text = "I love progamming and machine learnig."
+blob = TextBlob(text)
+corrected_text = blob.correct()
+print("Original Text:", text)
+print("Corrected Text:", corrected_text)
