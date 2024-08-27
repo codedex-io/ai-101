3de83b2ee0446ca09b25df66ad9f9f013b5ac753
Kat <21105989+ksolis3197@users.noreply.github.com>
2024-08-27 12:02:19 -0400

    Update 04_n_grams.py
    spacing update

--- 04_n_grams.py
+++ 04_n_grams.py
@@ -5,6 +5,7 @@ from collections import Counter
 nltk.download('punkt')
 sample_text = "I am learning NLP( Natural Lanaguage Processing)"
 tokens = word_tokenize(sample_text.lower())
+
 # Unigram
 unigrams = list(ngrams(tokens, 1))
 print("Unigrams:", unigrams)
